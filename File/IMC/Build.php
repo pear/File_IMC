e5918efe71a77686738d440ecaa4d2c5b82a56e5
Paul M Jones <pmjones@ciaweb.net>
2003-11-13 01:20:58 +0000

    Formatting changes to bring in line with PEAR CS, changes to comment formatting, moved is_a() to File_IMC "master" class, commented the raiseError() method.

--- File/IMC/Build.php
+++ File/IMC/Build.php
@@ -13,80 +13,104 @@
 // | obtain it through the world-wide-web, please send a note to          | 
 // | license@php.net so we can mail you a copy immediately.               | 
 // +----------------------------------------------------------------------+ 
-// | Authors: Paul M. Jones <pjones@ciaweb.net>                           | 
+// | Authors: Paul M. Jones <pmjones@ciaweb.net>                           | 
 // +----------------------------------------------------------------------+ 
 // 
 // $Id$ 
 
 /**
- * This class helps build files in the vCard and vCalendar formats.
- *
- * General note: we use the terms "set" "add" and "get" as function
- * prefixes.
- * 
- * "Set" means there is only one iteration of a component, and it has
- * only one value repetition, so you set the whole thing at once.
- * 
- * "Add" means eith multiple iterations of a component are allowed, or
- * that there is only one iteration allowed but there can be multiple
- * value repetitions, so you add iterations or repetitions to the current
- * stack.
- * 
- * "Get" returns the full vCard line for a single iteration.
- * 
- * @author Paul M. Jones <pjones@ciaweb.net>
- * @package File_IMC
- */
+* 
+* This class helps build files in the vCard and vCalendar formats.
+*
+* General note: we use the terms "set" "add" and "get" as function
+* prefixes.
+* 
+* "Set" means there is only one iteration of a component, and it has
+* only one value repetition, so you set the whole thing at once.
+* 
+* "Add" means either multiple iterations of a component are allowed, or
+* that there is only one iteration allowed but there can be multiple
+* value repetitions, so you add iterations or repetitions to the current
+* stack.
+* 
+* "Get" returns the full vCard line for a single iteration.
+* 
+* @author Paul M. Jones <pmjones@ciaweb.net>
+* 
+* @package File_IMC
+* 
+*/
 
 class File_IMC_Build {
 
-   /**
+    /**
+    * 
     * Values for vCard components.
     * 
     * @access public
+    * 
     * @var array
+    * 
     */
+    
     var $value = array();
+    
      
-   /**
+    /**
+    * 
     * Parameters for vCard components.
     * 
     * @access public
+    * 
     * @var array
+    * 
     */
+    
     var $param = array();
-       
-   /**
+    
+    
+    /**
+    * 
     * Tracks which component (N, ADR, TEL, etc) value was last set or added.
     * Used when adding parameters automatically to the proper component.
     *
     * @var string
     */
+    
     var $autoparam = null;
-
-   /**
+    
+    
+    /**
+    * 
     * Constructor
     *
     * @param string $version The vCard version to build; affects which
     * parameters are allowed and which components are returned by
     * fetch().
+    * 
     * @return void
     *
     * @see fetch()
+    * 
     */
-    function File_IMC_Build_vCard($version = '3.0') {
+    
+    function File_IMC_Build_vCard($version = '3.0')
+    {
         $this->reset($version);
     }
 
     
-   /**
+    /**
+    * 
     * Resets the vCard values and params to be blank.
     * 
     * @param string $version The vCard version to reset to ('2.1' or
     * '3.0' -- default is the same version as previously set).
+    * 
     * @return void
     * 
     * @access public
+    * 
     */
     
     function reset($version = null)
@@ -104,17 +128,23 @@ class File_IMC_Build {
         }
     }
     
-   /**
+    
+    /**
+    * 
     * Sets the version of the specification to use.  Only one iteration.
     *
     * Overload this function in the driver to validate and set the version
     * 
     * @param string $text The text value of the verson text (e.g. '3.0' or '2.1').
+    * 
     * @return mixed Void on success, or a PEAR_Error object on failure.
     * 
     * @access public
+    * 
     * @abstract
+    * 
     */
+    
     function setVersion($text)
     {
         $this->autoparam = 'VERSION';
@@ -127,20 +157,26 @@ class File_IMC_Build {
         }
     }
     
-   /**
+    
+    /**
+    * 
     * Gets back the version of the the vCard.  Only one iteration.
     * 
     * @return string The data-source of the vCard.
     *
     * @access public
+    * 
     */
+    
     function getVersion()
     {
         return $this->getMeta('VERSION', 0) .
             $this->getValue('VERSION', 0);
     }
     
-   /**
+    
+    /**
+    * 
     * Prepares a string so it may be safely used as vCard values.  DO
     * NOT use this with binary encodings.  Operates on text in-place;
     * does not return a value.  Recursively descends into arrays.
@@ -151,10 +187,13 @@ class File_IMC_Build {
     *     newline => literal \n
     * 
     * @param  mixed  $text The string or array or strings to escape.
+    * 
     * @return mixed        Void on success, or a PEAR_Error object on failure.
     * 
     * @access public
+    * 
     */
+    
     function escape(&$text)
     {
         if (is_object($text)) {
@@ -187,15 +226,16 @@ class File_IMC_Build {
         }
     }
 
-   /**
+    /**
+    * 
     * Adds a parameter value for a given component and parameter name.
-    *
-    * Note that although vCard 2.1 and vCalendar allow you to specify a 
-    * parameter value without a name (e.g., "HOME" instead of "TYPE=HOME") this
-    * class is not so lenient.  ;-)    You must specify a parameter name
-    * (TYPE, ENCODING, etc) when adding a parameter.  Call multiple
-    * times if you want to add multiple values to the same parameter.
-    * E.g.:
+    * 
+    * Note that although vCard 2.1 and vCalendar allow you to specify a
+    * parameter value without a name (e.g., "HOME" instead of
+    * "TYPE=HOME") this class is not so lenient.  ;-)    You must
+    * specify a parameter name (TYPE, ENCODING, etc) when adding a
+    * parameter.  Call multiple times if you want to add multiple values
+    * to the same parameter. E.g.:
     * 
     * $vcard = File_IMC::build('vCard');
     *
@@ -203,23 +243,29 @@ class File_IMC_Build {
     * $vcard->addParam('TYPE', 'HOME', 'TEL', 0);
     * $vcard->addParam('TYPE', 'PREF', 'TEL', 0);
     * 
-    * @param  string $param_name The parameter name, such as TYPE, VALUE,
-    *                            or ENCODING.
-    * @param  string $param_value The parameter value.
-    * @param  string $comp The vCard component for which this is a
-    *                      paramter (ADR, TEL, etc).  If null, will be the 
-    *                      component that was last set or added-to.
-    * @param  mixed  $iter An integer vCard component iteration that this
-    *                      is a param for.  E.g., if you have more than one 
-    *                      ADR component, 0 refers to the first ADR, 1 to
-    *                      the second ADR, and so on.  If null, the parameter
-    *                      will be added to the last component iteration 
-    *                      available.
-    * @return mixed        Void on success, or a PEAR_Error object on failure.
-    *
+    * @param string $param_name The parameter name, such as TYPE, VALUE,
+    * or ENCODING.
+    * 
+    * @param string $param_value The parameter value.
+    * 
+    * @param string $comp The vCard component for which this is a
+    * paramter (ADR, TEL, etc).  If null, will be the component that was
+    * last set or added-to.
+    * 
+    * @param mixed $iter An integer vCard component iteration that this
+    * is a param for.  E.g., if you have more than one ADR component, 0
+    * refers to the first ADR, 1 to the second ADR, and so on.  If null,
+    * the parameter will be added to the last component iteration
+    * available.
+    * 
+    * @return mixed Void on success, or a PEAR_Error object on failure.
+    * 
     * @access public
+    * 
     */
-    function addParam($param_name, $param_value, $comp = null, $iter = null)
+    
+    function addParam($param_name, $param_value, $comp = null,
+        $iter = null)
     {
         // if component is not specified, default to the last component
         // that was set or added.
@@ -256,40 +302,55 @@ class File_IMC_Build {
         }
     }
     
-   /**
+    
+    /**
+    * 
     * Validates parameter names and values
     * 
-    * @param  string $name The parameter name (e.g., TYPE or ENCODING).
-    * @param  string $text The parameter value (e.g., HOME or BASE64).
-    * @param  string $comp Optional, the component name (e.g., ADR or
-    *                      PHOTO).  Only used for error messaging.
-    * @param  string $iter Optional, the iteration of the component. 
-    *                      Only used for error messaging.
-    * @return mixed        Boolean true if the parameter is valid, or a
-    *                      PEAR_Error object if not.
+    * @param string $name The parameter name (e.g., TYPE or ENCODING).
+    * 
+    * @param string $text The parameter value (e.g., HOME or BASE64).
+    * 
+    * @param string $comp Optional, the component name (e.g., ADR or
+    * PHOTO). Only used for error messaging.
+    * 
+    * @param string $iter Optional, the iteration of the component. Only
+    * used for error messaging.
+    * 
+    * @return mixed Boolean true if the parameter is valid, or a
+    * PEAR_Error object if not.
     * 
     * @access public
+    * 
     * @abstract
-    */    
+    * 
+    */
+      
     function validateParam($name, $text, $comp = null, $iter = null)
     {
         // this must be overloaded in the drivers!
     }
-
-   /**
+    
+    
+    /**
+    * 
     * Gets the left-side/prefix/before-the-colon (metadata) part of a
     * vCard line, including the component identifier, the parameter
     * list, and a colon.
     * 
-    * @param  string $comp The component to get metadata for (ADR, TEL, etc).
-    * @param  int    $iter The vCard component iteration to get the metadata
-    *                      for.  E.g., if you have more than one ADR component, 
-    *                      0 refers to the first ADR, 1 to the second ADR, and 
-    *                      so on.
+    * @param string $comp The component to get metadata for (ADR, TEL,
+    * etc).
+    * 
+    * @param int $iter The vCard component iteration to get the metadata
+    * for. E.g., if you have more than one ADR component, 0 refers to
+    * the first ADR, 1 to the second ADR, and so on.
+    * 
     * @return string The line prefix metadata.
     * 
     * @access public
+    * 
     */
+    
     function getMeta($comp, $iter = 0)
     {
         $params = $this->getParam($comp, $iter);
@@ -305,22 +366,31 @@ class File_IMC_Build {
         return $text;
     }
     
-   /**
+    
+    /**
+    * 
     * Generic, all-purpose method to store a string or array in
     * $this->value, in a way suitable for later output as a vCard
     * element.  This forces the value to be the passed text or array
     * value, overriding any prior values.
     * 
-    * @param  string $comp The component to set the value for ('N', 'ADR', etc).
-    * @param  int    $iter The component-iteration to set the value for.
-    * @param  int    $part The part number of the component-iteration to set
-    *                      the value for.
-    * @param  mixed  $text A string or array; the set of repeated values
-    *                      for this component-iteration part.
+    * @param string $comp The component to set the value for ('N',
+    * 'ADR', etc).
+    * 
+    * @param int $iter The component-iteration to set the value for.
+    * 
+    * @param int $part The part number of the component-iteration to set
+    * the value for.
+    * 
+    * @param mixed $text A string or array; the set of repeated values
+    * for this component-iteration part.
+    * 
     * @return void
     * 
     * @access public
+    * 
     */
+    
     function setValue($comp, $iter, $part, $text)
     {
         $comp = strtoupper($comp);
@@ -329,23 +399,31 @@ class File_IMC_Build {
         $this->autoparam = $comp;
     }
     
-   /**
+    
+    /**
+    * 
     * Generic, all-purpose method to add a repetition of a string or
     * array in $this->value, in a way suitable for later output as a
     * vCard element.  This appends the value to be the passed text or
     * array value, leaving any prior values in place.
     * 
-    * @param  string $comp The component to set the value for ('N',
-    *                      'ADR', etc).
-    * @param  int    $iter The component-iteration to set the value for.
-    * @param  int    $part The part number of the component-iteration to set
-    *                      the value for.
-    * @param  mixed  $text A string or array; the set of repeated values
-    *                      for this component-iteration part.
+    * @param string $comp The component to set the value for ('N',
+    * 'ADR', etc).
+    * 
+    * @param int $iter The component-iteration to set the value for.
+    * 
+    * @param int $part The part number of the component-iteration to set
+    * the value for.
+    * 
+    * @param mixed $text A string or array; the set of repeated values
+    * for this component-iteration part.
+    * 
     * @return void
     * 
     * @access public
+    * 
     */
+    
     function addValue($comp, $iter, $part, $text)
     {
         $comp = strtoupper($comp);
@@ -356,23 +434,29 @@ class File_IMC_Build {
         $this->autoparam = $comp;
     }
     
-   /**
+    /**
+    * 
     * Generic, all-purpose method to get back the data stored in $this->value.
     * 
-    * @param  string $comp The component to set the value for ('N',
-    *                      'ADR', etc).
-    * @param  int    $iter The component-iteration to set the value for.
-    * @param  int    $part The part number of the component-iteration to get
-    *                      the value for.
-    * @param  mixed  $rept The repetition number within the part to get;
-    *                      if null, get all repetitions of the part within the 
-    *                      iteration.
-    * @return string       The value, escaped and delimited, of all
-    *                      repetitions in the component-iteration part (or 
-    *                      specific repetition within the part).
+    * @param string $comp The component to set the value for ('N',
+    * 'ADR', etc).
+    * 
+    * @param int $iter The component-iteration to set the value for.
+    * 
+    * @param int $part The part number of the component-iteration to get
+    * the value for.
+    * 
+    * @param mixed $rept The repetition number within the part to get;
+    * if null, get all repetitions of the part within the iteration.
+    * 
+    * @return string The value, escaped and delimited, of all
+    * repetitions in the component-iteration part (or specific
+    * repetition within the part).
     * 
     * @access public
+    * 
     */
+    
     function getValue($comp, $iter = 0, $part = 0, $rept = null)
     {
         if ($rept === null &&
@@ -397,18 +481,24 @@ class File_IMC_Build {
         }
     }
     
-   /**
+    
+    /**
+    * 
     * Gets back the parameter string for a given component.
     * 
-    * @param  string $comp The component to get parameters for (ADR, TEL, etc).
-    * @param  int    $iter The vCard component iteration to get the param
-    *                      list for.  E.g., if you have more than one ADR 
-    *                      component, 0 refers to the first ADR, 1 to the 
-    *                      second ADR, and so on.
+    * @param string $comp The component to get parameters for (ADR, TEL,
+    * etc).
+    * 
+    * @param int $iter The vCard component iteration to get the param
+    * list for. E.g., if you have more than one ADR component, 0 refers
+    * to the first ADR, 1 to the second ADR, and so on.
+    * 
     * @return string
     * 
     * @access public
+    * 
     */
+    
     function getParam($comp, $iter = 0)
     {
         $comp = strtoupper($comp);
@@ -450,6 +540,7 @@ class File_IMC_Build {
         return $text;
     }
     
+    
     /**
     * 
     * Builds a vCard/vCal from a parser result array.  Only send
@@ -464,12 +555,17 @@ class File_IMC_Build {
     * $vcard->setFromArray($info[0]); // [0] is the first card
     * 
     * @param  array  $src One vCard entry as parsed using File_IMC::parse()
+    * 
     * @return void
     * 
     * @see File_IMC_Parse::fromFile()
+    * 
     * @see File_IMC_Parse::fromText()
+    * 
     * @access public
+    * 
     */
+    
     function setFromArray($src)
     {
         // reset to a blank values and params
@@ -510,36 +606,43 @@ class File_IMC_Build {
         }
     }
     
-   /**
+    
+    /**
+    * 
     * Fetches a full vCard/vCal text block based on $this->value and
     * $this->param.
     *
     * @return string A properly formatted vCard/vCalendar text block.
     *
     * @access public
+    * 
     */
+    
     function fetch()
     {
         // this method must be overloaded in a driver!
     }
-
+    
+    
+    /**
+    * 
+    * Raises PEAR errors.
+    *
+    * @param string $msg The text error message.
+    *
+    * @param int $code The error code.
+    *
+    * @return void
+    *
+    * @access public
+    * 
+    */
+    
     function raiseError($msg, $code)
     {
         include_once 'PEAR.php';
-
         PEAR::raiseError($msg, $code);
     }
 
 }
-
-if (!function_exists('is_a')) {
-    /**
-     * Emulate is_a() for PHP < 4.2.0
-     */
-    function is_a($obj, $classname)
-    {
-        return get_class($obj) == strtolower($classname) || is_subclass_of($obj, $classname);
-    }
-}
-
 ?>
